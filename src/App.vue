6e33eb10bb01978fa78bf340ed708f26c679cad8
aGiser0 <12932593+agiser0@user.noreply.gitee.com>
2024-12-31 14:36:49 +0800

    fix: text2

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <script setup lang="ts">
-	const str = 'sdjak';;;
+const str = 'sdjak'
 </script>
 
 <template>
-	<h1>
-		根目录
-		<h2>dsf</h2>
-		<h2>dsf</h2>
-		<h2>dsf</h2>
-	</h1>
+  <h1>
+    根目录
+    <h2>dsf</h2>
+    <h2>dsf</h2>
+    <h2>dsf</h2>
+    <h2>dsf</h2>
+    <h2>dsf</h2>
+    <h2>dsf</h2>
+    <h2>dsf</h2>
+  </h1>
 </template>
 
 <style scoped lang="scss">
-	h1 {
-		width: 600px, h2 {
-			height: 300px;
-		}
-	}
+h1 {
+
+  width: 600px,
+  h2 {
+    height: 300px;
+  }
+}
 </style>
